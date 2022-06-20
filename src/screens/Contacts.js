483780d03cb63e79e1579daf0d4a9622a2a01980
Karen <zoroglian.karen@gmail.com>
2022-06-20 13:07:57 +0400

    adding new directory screens

---
+++ src/screens/Contacts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Source from "../components/Source";
+import "./Contacts.css";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import EmailIcon from "@mui/icons-material/Email";
+
+function Contacts() {
+  return (
+    <div className="body__container">
+      <Source url="https://t.me/samovolkin" Icon={TelegramIcon} />
+      <Source url="maksim@samovolkin.com" Icon={EmailIcon} />
+    </div>
+  );
+}
+
+export default Contacts;
